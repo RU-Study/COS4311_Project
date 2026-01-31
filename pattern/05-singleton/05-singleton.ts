5ada757b903dcb09ce673315ce0f73c9a2560359
Supanat_T. <82473731+Supanat-T02@users.noreply.github.com>
2026-01-31 15:28:55 +0700

    Move client code to separate client files
    Extracted demo/client code from pattern/05-singleton/05-singleton.ts and pattern/06-adapter/06-adapter.ts into new files pattern/05-singleton/client.ts and pattern/06-adapter/client.ts. The original module files now export their public classes (ComputerClock; THDate, Timestamp, ISO8601, TimestampAdapter, ISO8601Adapter) so the clients can import them, separating example usage from implementation.

--- pattern/05-singleton/05-singleton.ts
+++ pattern/05-singleton/05-singleton.ts
@@ -63,37 +63,4 @@ class ComputerClock {
   }
 }
 
-// + ------------ Client ------------ + //
-function addZero(num: number): string {
-  return ('00' + num).slice(-2);
-}
-
-function printDate(clock: ComputerClock): void {
-  console.log('------------------------------\n');
-  console.log(`วันที่: ${addZero(clock.getDay())}/${addZero(clock.getMonth())}/${addZero(clock.getYear())}`);
-  console.log(`เวลา: ${addZero(clock.getHours())}:${addZero(clock.getMinutes())}:${addZero(clock.getSeconds())}`);
-  console.log('\n------------------------------');
-}
-
-const clock1 = ComputerClock.getClock();
-
-clock1.setDate(19, 12, 2025);
-clock1.setTime(17, 30, 21);
-
-console.log('------------------------------');
-console.log('Clock 1');
-printDate(clock1);
-
-const clock2 = ComputerClock.getClock();
-
-console.log('Clock 2');
-printDate(clock2);
-
-clock2.setDate(1, 1, 2026);
-clock2.setTime(0, 0, 0);
-
-console.log('Clock 2');
-printDate(clock2);
-
-console.log('Clock 1');
-printDate(clock1);
+export { ComputerClock };
